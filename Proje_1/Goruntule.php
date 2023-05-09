21fc57e40472457f2693882af5c2b81e97020ccc
Mstafa0808 <104256048+Mstafa0808@users.noreply.github.com>
2023-05-09 22:43:07 +0300

    Add files via upload

---
+++ Proje_1/Goruntule.php
@@ -0,0 +1,13 @@
+<?php
+
+$Email=$_POST['email'];
+$Kullanici=$_POST['AdiSoyadi'];
+$Adres=$_POST['adres'];
+$Sehir=$_POST['Sehir'];
+$Cinsiyet=$_POST['cinsiyet'];
+
+
+    echo $Email.$Kullanici.$Adres.$Sehir.$Cinsiyet;
+
+
+?>
