7583d666bdb24084912b1b58f3739d51ff131913
Mstafa0808 <104256048+Mstafa0808@users.noreply.github.com>
2023-05-11 22:03:24 +0300

    Add files via upload

---
+++ Proje_3/asdsd.php
@@ -0,0 +1,46 @@
+<?php
+
+    if($_POST)
+    {
+
+    }
+    else
+    {
+
+    }
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Giriş</title>
+</head>
+<body>
+    
+    <table>
+            <tr>
+                <td>
+                    Kullanıcı Adı
+                </td>
+                <td>
+                <form><input type="email" name="KullaniciAdi" required placeholder="emailinizi giriniz"></form>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Şifre
+                </td>
+                <td>
+                    <form><input type="password" name="sifre"></form>
+                </td>
+            </tr>
+        </table>
+
+
+</body>
+</html>
